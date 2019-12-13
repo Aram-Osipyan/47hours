a72431670093428c74d079400db032f036597b79
Edcurrock <52678125+Edcurrock@users.noreply.github.com>
2019-12-13 22:10:25 +0300

    Enemy logic

--- project/Enemy.gd
+++ project/Enemy.gd
@@ -1,13 +1,17 @@
 extends KinematicBody2D
 const UP = Vector2(0,-1)
 const GRAVITY = 20
-const MAX_SPEED = 200
+const MAX_SPEED = 100
 const ACCELERATION = 50
 const MAX_JUMP_HEIGHT = -300
-var motion = Vector2()
+var velocity = Vector2()
 onready var Player = get_tree().get_root().get_node("Main/Player")
+# velocity = Vector2()
 func _physics_process(delta):
-	motion.y +=GRAVITY
-	
-	motion = move_and_slide(motion,UP)
+	velocity.y +=GRAVITY
+
+	if is_on_floor():
+		velocity.x = Player.get_global_position().x - get_global_position().x
+		velocity.normalized()
+	velocity = move_and_slide(velocity,UP)
 		
