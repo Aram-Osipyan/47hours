66dcd140b5afcbd1bf83c106d18be00a10668296
Edcurrock <52678125+Edcurrock@users.noreply.github.com>
2019-12-14 00:42:17 +0300

    Jump fix

--- project/Enemy.gd
+++ project/Enemy.gd
@@ -3,20 +3,22 @@ const UP = Vector2(0,-1)
 const GRAVITY = 20
 const MAX_SPEED = 100
 const ACCELERATION = 50
-const MAX_JUMP_HEIGHT = -200
+const MAX_JUMP_HEIGHT = -400
 var velocity = Vector2()
-var vel = Vector2()
 onready var Player = get_tree().get_root().get_node("Main/Player")
-# velocity = Vector2()
+var last_vel = Vector2()
+var jump_cond = true
 func _physics_process(delta):
-	velocity.y +=GRAVITY
-
-	
+	last_vel.x = velocity.x
+	velocity.y += GRAVITY
 	velocity.x = Player.get_global_position().x - get_global_position().x
-	velocity.normalized()
-	#print("1",velocity.x, "          ",Player.get_global_position().x)
-	if is_on_wall():
-		#print(velocity.x, "          ",Player.get_global_position().x)
+	print(velocity.x,"   ", last_vel.x)
+	if abs(velocity.x - last_vel.x) < 0.01:
+		jump_cond = false
+	#velocity.normalized()
+	if is_on_wall() and is_on_floor() and jump_cond :
 		velocity.y = MAX_JUMP_HEIGHT
+		
+	
 	velocity = move_and_slide(velocity,UP)
 		
