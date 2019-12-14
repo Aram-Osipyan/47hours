ad6be21d0be7863d99eecb25fe4a8e8a4075f331
Осипян Арам <osipyan.aram2017@ya.ru>
2019-12-15 01:21:31 +0300

    commit

--- project/Area2D.gd
+++ project/Area2D.gd
@@ -1,8 +1,8 @@
 extends Area2D
-enum Banks {RED =0 ,GREEN=1,BLUE=2}
+#enum Banks {RED =0 ,GREEN=1,BLUE=2}
 signal my_signal(x)
-
-export var resourse = 0
+export var is_big = true
+export var color_id = 1
 
 #<source_node>.connect(<signal_name>, <target_node>, <target_function_name>)
 func _ready():
@@ -10,9 +10,11 @@ func _ready():
 	
 func _on_Area2D_body_entered(body):
 	if(body.name == "Player"):
-
-	#body.get_node("Light2D").set_visible(true)
-		emit_signal("my_signal",resourse)
+	
+		#body.get_node("Light2D").set_visible(true)
+		#emit_signal("my_signal",is_big)
+		get_parent().call("add_resource",is_big,color_id)
+		print("on_collide")
 		queue_free()
 		
 
--- project/UI/resouces.gd
+++ project/UI/resouces.gd
@@ -8,6 +8,7 @@ signal kraft
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.connect("kraft",get_parent().get_parent(),"to_kraft")
+	$ProgressBar.value = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -15,7 +16,7 @@ func _ready():
 
 
 func _on_Button_pressed():
-	get_node("CanvasLayer/Button").hide()	
+	#get_node("CanvasLayer/Button").hide()	
 	get_node("light_lifebar").call("my_init")
 	get_node("light_lifebar").show()
 	emit_signal("kraft")
