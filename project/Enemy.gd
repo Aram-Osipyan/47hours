0d9b794979084b503d686beb7824a2d9496b174c
Edcurrock <52678125+Edcurrock@users.noreply.github.com>
2019-12-14 14:32:45 +0300

    add secret_treasure remind Enemy

--- project/Enemy.gd
+++ project/Enemy.gd
@@ -6,6 +6,7 @@ const ACCELERATION = 50
 const MAX_JUMP_HEIGHT = -400
 var velocity = Vector2()
 onready var Player = get_tree().get_root().get_node("Main/Player")
+onready var Ray = $RayCast2D
 var last_vel = Vector2()
 var flag = false
 
@@ -14,6 +15,7 @@ func movement():
 	
 	velocity.y += GRAVITY
 	velocity.x = Player.get_global_position().x - get_global_position().x
+	
 	if velocity.x <0:
 		$Sprite.flip_h = true
 		$Sprite.play("move")
@@ -22,20 +24,39 @@ func movement():
 		$Sprite.play("move")
 	
 	
-	if abs(get_global_position().x - last_vel.x) <0.13:
-
+	if abs(get_global_position().x - last_vel.x) <0.14:
 		jump_cond = false
-	velocity.normalized()
+		
+	
+	
 	if is_on_wall() and is_on_floor() and jump_cond :
 		velocity.y = MAX_JUMP_HEIGHT
 		
 	last_vel.x = get_global_position().x
+	#velocity.x = velocity.x.normalized()
+	
+	if(velocity.x <0):
+		Ray.rotation = -PI
+	else:
+		Ray.rotation = 0
+	
+	
 	velocity = move_and_slide(velocity,UP)
+	if velocity and get_slide_count()!=0:
+		for i in get_slide_count():
+			var collision = get_slide_collision(i)
+			if collision and collision.get_collider().name == "Player":
+				queue_free()
 func _physics_process(delta):
 	if flag:
 		movement()
-		
-
+		#Ray.set_enabled(true)
+	#else:
+	#	Ray.set_enabled(false)
+	#if Ray.is_colliding():
+	#	if Ray.get_collider().name == "Player":
+		#	queue_free()
+	
 
 func _on_Area_body_entered(body):
 	if(body.name == "Player"):
